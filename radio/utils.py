5881cb1fc0fff21d9b974dde3b3a39eb5a3fbddc
Ivan Habunek <ivan@habunek.com>
2017-01-06 12:11:55 +0100

    Add scraper for Radio 101

--- radio/utils.py
+++ radio/utils.py
@@ -1,9 +1,24 @@
 import requests
 
+from datetime import datetime
 
-def get_current_song(radio_slug):
+
+def get_current_song(slug):
+    if slug == 'antena':
+        return _prvi('antena')
+
+    if slug == 'gold':
+        return _prvi('gold')
+
+    if slug == 'radio101':
+        return _radio101()
+
+    raise ValueError("Unknown radio '{}'".format(slug))
+
+
+def _prvi(slug):
     url = 'http://www.prvi.hr/modules/mod_radioplayer/tmpl/icecast_fireplay.php'
-    data = {"song_cache_dirname": radio_slug}
+    data = {"song_cache_dirname": slug}
 
     response = requests.post(url, data=data)
     data = response.json()
@@ -11,4 +26,17 @@ def get_current_song(radio_slug):
     return [
         data['artist'],
         data['song'],
+    ]
+
+
+def _radio101():
+    url = 'http://www.radio101.hr/generated/radio_playlist.json'
+    params = {"request.preventCache": datetime.now().timestamp()}
+
+    response = requests.get(url, params)
+    data = response.json()
+
+    return [
+        data[0]['author'],
+        data[0]['title'],
     ]
