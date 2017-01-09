fcaa166f23ffa0aeb5d88b2ce7f01aeffd59fdb4
Ivan Habunek <ivan@habunek.com>
2017-01-09 10:01:35 +0100

    Set ALLOWED_HOSTS from env

--- radioscraper/settings.py
+++ radioscraper/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import dj_database_url
 import os
-from .env import ENV_BOOL, ENV_STR
+from .env import ENV_BOOL, ENV_STR, ENV_LIST
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +25,7 @@ SECRET_KEY = ENV_STR('SECRET_KEY', None)
 
 DEBUG = ENV_BOOL('DEBUG', False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ENV_LIST('ALLOWED_HOSTS', ',', [])
 
 INSTALLED_APPS = [
     'django.contrib.admin',
