d5f04508d0ec1b549e24e4f7a35946efe5cd219a
Ivan Habunek <ivan@habunek.com>
2017-01-12 15:20:21 +0100

    Autoload new radios on deploy

--- fabfile.py
+++ fabfile.py
@@ -13,4 +13,5 @@ def deploy():
         run("git pull")
         run("_env/bin/pip install -r requirements.txt")
         run("source .env; _env/bin/python manage.py migrate")
+        run("source .env; _env/bin/python manage.py loaddata radios.json")
         run("sudo service radioscraper reload")
