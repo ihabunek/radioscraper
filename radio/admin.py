2c5637af199c1f5a628dbe6e922b4087e73aacfe
Ivan Habunek <ivan@habunek.com>
2017-02-02 15:20:36 +0100

    Add admin pages

--- radio/admin.py
+++ radio/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Play, Radio
 
-# Register your models here.
+
+@admin.register(Play)
+class PlayAdmin(admin.ModelAdmin):
+    list_display = ('id', 'radio', 'artist_name', 'title', 'timestamp')
+    list_filter = ('radio',)
+
+
+@admin.register(Radio)
+class RadioAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'play_count', 'since', 'active')
+    ordering = ('name',)
+
+    def play_count(self, radio):
+        return radio.plays().count()
+
+    def since(self, radio):
+        return radio.first_play().timestamp
