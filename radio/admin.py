2d256e12f78958aca523a594ea35dc21c0fc23f3
Ivan Habunek <ivan@habunek.com>
2017-10-31 11:44:46 +0100

    Minor fixes to admin

--- radio/admin.py
+++ radio/admin.py
@@ -4,17 +4,34 @@ from .models import Play, Radio
 
 @admin.register(Play)
 class PlayAdmin(admin.ModelAdmin):
-    list_display = ('id', 'radio', 'artist_name', 'title', 'timestamp')
+    list_display = (
+        'id',
+        'radio',
+        'artist_name',
+        'title',
+        'timestamp',
+    )
+
     list_filter = ('radio',)
 
 
 @admin.register(Radio)
 class RadioAdmin(admin.ModelAdmin):
-    list_display = ('name', 'slug', 'play_count', 'since', 'active')
+    list_display = (
+        'name',
+        'slug',
+        'play_count',
+        'since',
+        'active',
+    )
+
+    readonly_fields = (
+        'first_play',
+        'last_play',
+        'play_count',
+    )
+
     ordering = ('name',)
 
-    def play_count(self, radio):
-        return radio.plays().count()
-
     def since(self, radio):
-        return radio.first_play().timestamp
+        return radio.first_play.timestamp
