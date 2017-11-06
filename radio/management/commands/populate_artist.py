e0852ffa1d9a417d6eaf804329a50f55ce8a68a8
Ivan Habunek <ivan@habunek.com>
2017-11-06 10:20:37 +0100

    Extended logging in populate_artist command

--- radio/management/commands/populate_artist.py
+++ radio/management/commands/populate_artist.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.core.management.base import BaseCommand
 from django.db.transaction import atomic
 
@@ -6,6 +8,12 @@ from radio.models import Play
 
 
 class Command(BaseCommand):
+    def process_name(self, name):
+        created, artist = get_or_create_artist(name)
+        Play.objects.filter(artist_name=name).update(artist=artist)
+        return created, artist
+
+
     def handle(self, *args, **options):
         names = (Play.objects
             .filter(artist__isnull=True)
@@ -14,9 +22,11 @@ class Command(BaseCommand):
             .distinct())
 
         count = names.count()
+        start = datetime.now()
 
         for ord, name in enumerate(names):
             with atomic():
-                created, artist = get_or_create_artist(name)
-                print("{}/{} {:50} {}".format(ord, count, name, "CREATED" if created else ""))
-                Play.objects.filter(artist_name=name).update(artist=artist)
+                created, artist = self.process_name(name)
+                duration = (datetime.now() - start).total_seconds()
+                speed = 1000 * duration / ord if ord else 0
+                print("{:5}/{} {:50} {:3} {:.1f} ms/item".format(ord, count, name, "NEW" if created else "", speed))
