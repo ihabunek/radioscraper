21d1b5025dc8d962986d6cc1762bd7a261cc0a75
Ivan Habunek <ivan@habunek.com>
2018-05-10 14:25:56 +0200

    Fix loaders for prvi.hr bunch of radios
    They changed the way currently played songs are shown.

--- loaders/implementations/narodni.py
+++ loaders/implementations/narodni.py
@@ -1,9 +1,20 @@
-from loaders.implementations.common import prvi
+from datetime import datetime
+from requests import Request
+from radio.utils.normalize import split_artist_title
+
+
+def _timestamp():
+    return int(datetime.now().timestamp() * 1000)
 
 
 def form_request():
-    return prvi.form_request('narodni')
+    url = 'http://streaming.narodni.hr/stream/now_playing.php'
+
+    return Request("GET", url, params={
+        'the_stream': 'http://live.narodni.hr:8059/;',
+        '_': _timestamp(),
+    })
 
 
 def parse_response(response):
-    return prvi.parse_response(response)
+    return split_artist_title(response.text)
