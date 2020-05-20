4846c486c811ff0e6f95d910753a562883490217
Ivan Habunek <ivan@habunek.com>
2020-05-20 16:47:07 +0200

    Add login view

--- radioscraper/urls.py
+++ radioscraper/urls.py
@@ -1,10 +1,13 @@
-from django.urls import path, include
-from django.contrib import admin
 from django.conf import settings
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.urls import path, include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', auth_views.LoginView.as_view(), name='logout'),
     path('', include('loaders.urls')),
     path('', include('music.urls')),
     path('', include('radio.urls')),
