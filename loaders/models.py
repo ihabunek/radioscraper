138e75ae4c9e16a9cf3d7ade602a45dd86c12294
Ivan Habunek <ivan@habunek.com>
2020-05-20 15:32:07 +0200

    Remove request and response data from failure

--- loaders/models.py
+++ loaders/models.py
@@ -13,29 +13,6 @@ class Outage(models.Model):
     failure_count = models.PositiveIntegerField(default=0)
 
 
-class RequestData(models.Model):
-    """
-    Holds HTTP request data.
-    """
-    method = models.CharField(max_length=10)
-    url = models.CharField(max_length=1000)
-    post_data = JSONField(blank=True, null=True)
-
-    def __repr__(self):
-        return "Request('{}', '{}')".format(self.method, self.url)
-
-
-class ResponseData(models.Model):
-    """
-    Holds HTTP response data.
-    """
-    status_code = models.PositiveSmallIntegerField()
-    contents = models.TextField()
-
-    def __repr__(self):
-        return "Response({})".format(self.status_code)
-
-
 class LoaderFailure(models.Model):
     """
     Contains the exception info which caused a loader to fail.
@@ -43,7 +20,5 @@ class LoaderFailure(models.Model):
     radio = models.ForeignKey('radio.Radio', on_delete=CASCADE)
     outage = models.ForeignKey(Outage, on_delete=SET_NULL, blank=True, null=True, related_name="failures")
     timestamp = models.DateTimeField(auto_now_add=True)
-    request = models.ForeignKey(RequestData, on_delete=PROTECT, blank=True, null=True)
-    response = models.ForeignKey(ResponseData, on_delete=PROTECT, blank=True, null=True)
     error_message = models.TextField(blank=True)
     stack_trace = models.TextField(blank=True)
