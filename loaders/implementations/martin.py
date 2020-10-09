9a41486953a57534dfc6c71bd294461e44faa086
Ivan Habunek <ivan@habunek.com>
2020-10-09 14:11:13 +0200

    Replace radio martin loader for new website

--- loaders/implementations/martin.py
+++ loaders/implementations/martin.py
@@ -1,52 +1,18 @@
-import re
-
 from radio.utils.normalize import split_artist_title
+
+from .common import timestamp_ms
 
 
 async def load(session):
-    # Old stream turned off, new stream is not up yet
-    # Stop loading until this is resolved
-    # https://stream.zabavni.hr/zabavni.html
-    return None
+    url = "https://stream.zabavni.hr/now_playing.php"
+    params = {
+        "the_stream": "https://test1.secure.com.hr:8585/;",
+        "_": timestamp_ms()
+    }
 
-    url = "http://genf196.server4you.de:8585/"
-    headers = {"Icy-MetaData": "1"}
+    response = await session.get(url, params=params)
+    contents = await response.text()
 
-    async with session.get(url, headers=headers) as response:
-        # Find chunk size, metadata is included after each chunk
-        offset = int(response.headers.get('icy-metaint'))
-
-        # Check sane offset value (usually 16k)
-        if not (1 < offset < 64 * 1024):
-            raise Exception(f"invalid icy-metaint value: {offset}")
-
-        # Skip first chunk
-        await response.content.readexactly(offset)
-
-        # Determine length of meta data from first byte
-        b = await response.content.readexactly(1)
-        length = b[0] * 16
-
-        # Check sane length value (usually 32)
-        if not (1 < length < 128):
-            raise Exception(f"invalid meta length: {length}")
-
-        # Read and decode metadata
-        meta = await response.content.readexactly(length)
-
-    meta = meta.decode("utf-8")
-    match = re.search("StreamTitle='(.+)';", meta)
-    if not match:
-        raise Exception(f"metadata not found in: {meta}")
-
-    parts = split_artist_title(match.group(1))
-    if parts is None:
-        return None
-
-    artist, title = parts
-
-    # Skip commercials
-    if artist.lower() == 'zabavni radio' and title.lower() == 'reklame':
-        return None
+    artist, title = split_artist_title(contents)
 
     return artist.title(), title
