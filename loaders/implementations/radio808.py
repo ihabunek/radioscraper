df1f1789914d4b9b300977091f19a426d31fdff6
Ivan Habunek <ivan@habunek.com>
2021-05-31 14:52:21 +0200

    Use full URL, don't trim the .html

--- loaders/implementations/radio808.py
+++ loaders/implementations/radio808.py
@@ -5,10 +5,9 @@ from radio.utils.normalize import split_artist_title
 
 logger = logging.getLogger(__name__)
 
-async def load(session):
-    url = 'https://808proxy.contrib.hr/7'
 
-    response = await session.get(url)
+async def load(session):
+    response = await session.get('https://808proxy.contrib.hr/7.html')
     contents = await response.text()
 
     match = re.search(r"<html><body>\d+,\d+,\d+,\d+,\d+,\d+,(.+)</body></html>", contents)
