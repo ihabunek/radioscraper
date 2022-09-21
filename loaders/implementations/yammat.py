89a457102afcf21ee9f6f75b7e874e83edb8a9a2
Ivan Habunek <ivan@habunek.com>
2022-09-21 18:46:18 +0200

    Skip yammat commercials

--- loaders/implementations/yammat.py
+++ loaders/implementations/yammat.py
@@ -27,4 +27,12 @@ async def load(session):
     if not artist or not title:
         return None
 
-    return spans
+    # Skip commercials
+    if artist.startswith("SHOP") or artist.startswith("SHCL"):
+        return None
+
+    # Skip whatever this is
+    if artist == "NO" and title == "UPDATE":
+        return None
+
+    return artist, title
