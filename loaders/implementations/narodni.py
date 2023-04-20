2f234c7e4201fbaba1af6bd304c2ee629c25bcbb
Ivan Habunek <ivan@habunek.com>
2023-04-20 13:14:29 +0200

    Remove loader for narodni

--- loaders/implementations/narodni.py
+++ loaders/implementations/narodni.py
@@ -1,15 +1,3 @@
-from radio.utils.normalize import split_artist_title
-
-from .common import timestamp_ms
-
-
 async def load(session):
-    url = 'http://streaming.narodni.hr/stream/now_playing.php'
-
-    response = await session.get(url, params={
-        'the_stream': 'http://live.narodni.hr:8059/;',
-        '_': timestamp_ms(),
-    })
-    contents = await response.text()
-
-    return split_artist_title(contents, normalize_case=True)
+    # Narodni is no more
+    return None
