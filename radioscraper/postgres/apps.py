8fe13731f52216b4ebec11a0787fe8fe35e97819
Ivan Habunek <ivan@habunek.com>
2023-05-18 19:21:59 +0200

    Remove deprecated ugettext_lazy

--- radioscraper/postgres/apps.py
+++ radioscraper/postgres/apps.py
@@ -1,14 +1,13 @@
 from django.apps import AppConfig
 from django.db.models import CharField, TextField
-from django.utils.translation import ugettext_lazy as _
 
 from .lookups import ImmutableUnaccent
 
 
 class PostgresConfig(AppConfig):
-    name = 'radioscraper.postgres'
-    label = 'postgres_addons'
-    verbose_name = _('PostgreSQL additions')
+    name = "radioscraper.postgres"
+    label = "postgres_addons"
+    verbose_name = "PostgreSQL additions"
 
     def ready(self):
         CharField.register_lookup(ImmutableUnaccent)
