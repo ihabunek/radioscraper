6e35ce6ddca16cc5abdc5875cad1df62fa6e7fb4
Ivan Habunek <ivan@habunek.com>
2024-06-28 16:19:33 +0200

    Fix bug in zabavni radio

--- loaders/implementations/zabavni.py
+++ loaders/implementations/zabavni.py
@@ -7,16 +7,25 @@ async def load(session):
     url = "https://stream.zabavni.hr/now_playing.php"
     params = {
         "the_stream": "https://genf196.server4you.de:8585;",
-        "_": timestamp_ms()
+        "_": timestamp_ms(),
     }
 
     response = await session.get(url, params=params)
     contents = await response.text()
 
-    artist, title = split_artist_title(contents)
+    # Handle error returned in HTTP 200 response
+    if contents == "The song title is not available":
+        return None
+
+    result = split_artist_title(contents)
+
+    if not result:
+        return None
+
+    artist, title = result
 
     # Skip commercials
-    if title == 'Reklame' and artist.title() == 'Zabavni Radio':
+    if title == "Reklame" and artist.title() == "Zabavni Radio":
         return None
-        
+
     return artist.title(), title
