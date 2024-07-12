7db76e4e563cfbaf829b05a8a1fcbd5fd19dce10
Ivan Habunek <ivan@habunek.com>
2024-07-12 14:39:21 +0200

    Fix korzo loader to skip empty title/artist combos

--- loaders/implementations/korzo.py
+++ loaders/implementations/korzo.py
@@ -3,15 +3,15 @@ from xml.etree import ElementTree
 
 async def load(session):
     response = await session.get("https://web.radiokorzo.hr/stream-info/si.xml")
-
     contents = await response.text()
     root = ElementTree.fromstring(contents)
 
     # The currently playing song has an Expire child element
     for song in root.findall("Song"):
         if song.find("Expire") is not None:
-            title = song.attrib["title"]
             artist_node = song.find("Artist")
             if artist_node is not None:
-                artist = artist_node.attrib["name"]
-                return [artist, title]
+                title = song.attrib.get("title")
+                artist = artist_node.attrib.get("name")
+                if artist and title:
+                    return [artist, title]
