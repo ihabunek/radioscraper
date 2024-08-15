39090b4d0bfe48ff4c48d3ed00b14f4f8f4442c0
Ivan Habunek <ivan@habunek.com>
2024-08-15 13:11:53 +0200

    Default to AutoField
    We don't want to migrate to BigAutoField soon, it takes a long while
    apply that migration to large tables.

--- radioscraper/settings/base.py
+++ radioscraper/settings/base.py
@@ -133,4 +133,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
