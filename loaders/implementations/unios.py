7e5394542f8fb17aa6c94c0f4003c7bbc9cbaee6
Ivan Habunek <ivan@habunek.com>
2024-08-15 12:52:38 +0200

    Use new endpoint for radio unios

--- loaders/implementations/unios.py
+++ loaders/implementations/unios.py
@@ -1,18 +1,16 @@
-import json
-
 from radio.utils.normalize import split_artist_title
 from .common import timestamp_ms
 
+
 async def load(session):
-    url = "http://radio.unios.hr:8000/json.xsl"
+    url = "http://radio.unios.hr:8000/status-json.xsl"
     response = await session.get(url, params={
         '_': timestamp_ms()
     })
-    jsonp_data = await response.text()
 
-    data = json.loads(jsonp_data.split("(", 1)[1].strip(");"))
-
-    artist_song = data["/fm.mp3"]["title"]
+    data = await response.json()
+    # Using [2] because that's what they do on the web site
+    artist_song = data["icestats"]["source"][2]["title"]
 
     if "OFF AIR" in artist_song or "Radio UNIOS" in artist_song:
         return None
