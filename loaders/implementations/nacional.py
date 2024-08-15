763d2135d5ecdb7979efe36387a81d11fb8076a3
Ivan Habunek <ivan@habunek.com>
2024-08-15 12:23:02 +0200

    Fix error when playing list has only one item

--- loaders/implementations/nacional.py
+++ loaders/implementations/nacional.py
@@ -19,7 +19,12 @@ async def load(session):
     response = await session.get("https://radio.nacional.hr/current-song")
     records = await response.json(content_type=None)  # ignore invalid header
 
-    _, (artist, title), _ = records[0]
+    _, playing, _ = records[0]
+
+    if not playing or len(playing) != 2:
+        return None
+
+    artist, title = playing
     artist = html.unescape(artist)
     title = html.unescape(title)
 
