0ccaecbbb8c278812401d4371b624fc4a4489c5a
Ivan Habunek <ivan@habunek.com>
2024-08-15 11:25:14 +0200

    Skip non-songs for nacional

--- loaders/implementations/nacional.py
+++ loaders/implementations/nacional.py
@@ -1,6 +1,19 @@
 import html
 
 
+SKIP_ARTISTS = [
+    "nacional",
+    "gradske novice",
+]
+
+SKIP_TITLES = [
+    "na krilima demokracije",
+    "promocija tjednika nacional",
+    "stanje u prometu",
+    "novice iz grada",
+]
+
+
 async def load(session):
     response = await session.get("https://radio.nacional.hr/current-song")
     records = await response.json(content_type=None)  # ignore invalid header
@@ -8,5 +21,11 @@ async def load(session):
     _, (artist, title), _ = records[0]
     artist = html.unescape(artist)
     title = html.unescape(title)
+
+    if title.lower() in SKIP_TITLES:
+        return None
+
+    if artist.lower() in SKIP_ARTISTS:
+        return None
 
     return artist.title(), title.capitalize()
