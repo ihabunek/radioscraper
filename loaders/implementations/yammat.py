ee0ae560b934d691ff5c77387b68212af8b76269
Ivan Habunek <ivan@habunek.com>
2024-08-17 11:08:18 +0200

    Remove yammat hacks, maybe no longer needed

--- loaders/implementations/yammat.py
+++ loaders/implementations/yammat.py
@@ -17,15 +17,4 @@ async def load(session):
     if not artist or not title:
         return None
 
-    # Skip commercials
-    if artist.startswith("SHOP") or artist.startswith("SHCL"):
-        return None
-
-    # Skip whatever this is
-    if artist == "NO" and title == "UPDATE":
-        return None
-
-    if artist.startswith("SELO MOJE MALO") or artist.startswith("VIJESTI DANA"):
-        return None
-
     return artist, title
