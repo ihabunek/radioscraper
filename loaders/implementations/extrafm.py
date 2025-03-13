2232d818003d9a744da999d55232504a39ee6505
Ivan Habunek <ivan@habunek.com>
2025-03-13 11:45:49 +0100

    Fix extra fm

--- loaders/implementations/extrafm.py
+++ loaders/implementations/extrafm.py
@@ -1,15 +1,7 @@
 from radio.utils.normalize import split_artist_title
 
-from .common import timestamp_ms
-
 
 async def load(session):
-    url = 'http://streaming.extrafm.hr/stream/now_playing.php'
-
-    response = await session.get(url, params={
-        'the_stream': 'http://streams.extrafm.hr:8110/;',
-        '_': timestamp_ms(),
-    })
+    response = await session.get("https://streaming.extrafm.hr/stream/info/listen_extra_aac_.txt")
     contents = await response.text()
-
     return split_artist_title(contents, normalize_case=True)
