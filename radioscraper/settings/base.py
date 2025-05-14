67147a102d4c323701ddb07c9b774eeb256383c4
Ivan Habunek <ivan@habunek.com>
2025-05-14 17:01:55 +0200

    Revert "Upgrade to Django 5.1"
    This reverts commit f51c6d44cda0029a99f6f5a127d0eedbe4ea3a6c.

--- radioscraper/settings/base.py
+++ radioscraper/settings/base.py
@@ -1,3 +1,4 @@
+import dj_database_url
 import os
 
 from radioscraper.postgres.lookups import ImmutableUnaccent  # noqa
@@ -74,13 +75,13 @@ WSGI_APPLICATION = 'radioscraper.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {}
+DATABASES = {'default': dj_database_url.config()}
 
 
 # Password validation
-# https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
+# https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -99,7 +100,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 # Internationalization
-# https://docs.djangoproject.com/en/5.1/topics/i18n/
+# https://docs.djangoproject.com/en/3.0/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
@@ -111,11 +112,14 @@ USE_L10N = False
 
 USE_TZ = True
 
+DATE_FORMAT = 'd.m.Y'
+DATETIME_FORMAT = 'd.m.Y @ H:i'
+
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
+# https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 # URL to use when referring to static files located in STATIC_ROOT.
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Additional locations the staticfiles app will traverse.
 STATICFILES_DIRS = [
@@ -127,6 +131,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
