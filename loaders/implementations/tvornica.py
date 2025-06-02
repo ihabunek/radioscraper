1696e422565a016e3088361176e5a0d050a7f4eb
Ivan Habunek <ivan@habunek.com>
2025-06-02 08:58:10 +0200

    Fix radio tvornica

--- loaders/implementations/tvornica.py
+++ loaders/implementations/tvornica.py
@@ -3,6 +3,6 @@ from aiohttp.client import ClientSession
 
 
 async def load(session: ClientSession):
-    response = await session.get("https://radiotvornica.hr/sys/ajax/stream-data.aspx")
+    response = await session.get("https://radiotvornica.hr/sys/ajax/stream-data.aspx", ssl=False)
     contents = await response.text()
     return split_artist_title(contents, normalize_case=True)
