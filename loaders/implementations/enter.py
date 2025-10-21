4e60bb7247daa073b9247f400476cccd9b8390d2
Ivan Habunek <ivan@habunek.com>
2025-10-21 17:47:51 +0200

    Attempt to fix enter

--- loaders/implementations/enter.py
+++ loaders/implementations/enter.py
@@ -1,15 +1,11 @@
 from radio.utils.normalize import split_artist_title
 
-from .common import timestamp_ms
-
-
 async def load(session):
-    url = 'http://streaming.enterzagreb.hr/main/now_playing.php'
-
-    response = await session.get(url, params={
-        'the_stream': 'http://live.enterzagreb.hr:8023/;',
-        '_': timestamp_ms(),
-    })
+    url = 'https://stream.enterzagreb.hr/player/info/listen_enter_mp3_256_.txt'
+    response = await session.get(url)
     contents = await response.text()
+
+    if contents == "Live Broadcast":
+        return None
 
     return split_artist_title(contents, normalize_case=True)
