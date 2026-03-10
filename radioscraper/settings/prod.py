ae5fdfd00988145e018cdc412e213924715d9852
Ivan Habunek <ivan@habunek.com>
2026-03-10 09:43:06 +0100

    Log to systemd in production

--- radioscraper/settings/prod.py
+++ radioscraper/settings/prod.py
@@ -1,8 +1,6 @@
 from .base import *  # noqa
 
-ALLOWED_HOSTS = ['www.radioscraper.com']
-
-ADMINS = [('Ivan Habunek', 'ivan@habunek.com')]
+ALLOWED_HOSTS = ["www.radioscraper.com"]
 
 # Much security
 # See `./manage.py check --deploy`
@@ -10,47 +8,43 @@ CSRF_COOKIE_SECURE = True
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_HSTS_PRELOAD = True
 SECURE_HSTS_SECONDS = 60 * 60
-SECURE_REFERRER_POLICY = 'same-origin'
+SECURE_REFERRER_POLICY = "same-origin"
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 
+# Logging to systemd
 LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'default': {
-            'format': '%(process)8d %(levelname)8s %(name)s %(message)s'
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "simple": {
+            "format": "{levelname} {name} {message}",
+            "style": "{",
         },
     },
-    'handlers': {
-        'console': {
-            'level': 'INFO',
-            'class': 'logging.StreamHandler',
-            'formatter': 'default'
+    "handlers": {
+        "journal": {
+            "class": "systemd.journal.JournalHandler",
+            "formatter": "simple",
+            "SYSLOG_IDENTIFIER": "radioscraper",
         },
-        'app': {
-            'level': 'INFO',
-            'class': 'logging.handlers.SysLogHandler',
-            'facility': 'local6',
-            'formatter': 'default',
-            'address': '/dev/log'
-        },
-        'loaders': {
-            'level': 'INFO',
-            'class': 'logging.handlers.SysLogHandler',
-            'facility': 'local7',
-            'formatter': 'default',
-            'address': '/dev/log'
-        }
     },
-    'loggers': {
-        'django.request': {
-            'handlers': ['app'],
-            'level': 'ERROR'
+    "loggers": {
+        "django": {
+            "handlers": ["journal"],
+            "level": "INFO",
+            "propagate": False,
         },
-        'loaders': {
-            'handlers': ['loaders'],
-            'level': 'INFO',
-        }
+        # Don't log http requests
+        "django.server": {
+            "handlers": ["journal"],
+            "level": "WARNING",
+            "propagate": False,
+        },
+        "radioscraper": {
+            "handlers": ["journal"],
+            "level": "INFO",
+            "propagate": False,
+        },
     },
 }
