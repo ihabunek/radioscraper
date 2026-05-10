bbd6cabbb8b5d98f47ed044724a784840af83400
Ivan Habunek <ivan@habunek.com>
2026-05-10 12:34:54 +0200

    Fix warning

--- music/admin.py
+++ music/admin.py
@@ -5,8 +5,7 @@ from music.models import Artist
 @admin.register(Artist)
 class ArtistAdmin(admin.ModelAdmin):
     list_display = (
-        'name',
-        'slug',
-        'names',
+        "name",
+        "slug",
     )
-    ordering = ('name',)
+    ordering = ("name",)
